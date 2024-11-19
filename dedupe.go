d473bb8b23e55d7b20ee3fe790fe14ebaa47ef37
phil <p@pkpowell.net>
2024-11-19 17:40:32 +0100

    updates

--- dedupe.go
+++ dedupe.go
@@ -1,9 +1,11 @@
 package dedupe
 
 import (
+	"cmp"
+	"fmt"
 	"slices"
-	"sort"
 	"strings"
+	"sync"
 )
 
 type Data struct {
@@ -32,6 +34,40 @@ func (d *Data) AddWord(word string) {
 
 func (d *Data) Deduped() []string {
 	return d.WordsDeduped
+}
+
+type Node struct {
+	Children Children      `json:"children,omitempty"`
+	Count    int           `json:"count,omitempty"`
+	mtx      *sync.RWMutex `json:"-"`
+}
+
+type Children map[string]*Node
+
+// newTrie initializes a new Trie
+func newTrie() *Node {
+	return &Node{
+		Children: make(Children),
+		// IsEnd:       false,
+		Count: 0,
+		mtx:   new(sync.RWMutex),
+	}
+}
+
+// Update updates a word in the trie
+func (root *Node) update(word string) {
+	root.mtx.Lock()
+	defer root.mtx.Unlock()
+
+	current := root
+
+	for _, letter := range word {
+		_, ok := current.Children[string(letter)]
+		if !ok {
+			current.Children[string(letter)] = newTrie()
+		}
+		current = current.Children[string(letter)]
+	}
 }
 
 // func (d *Data) Contains(word []string) {
@@ -66,20 +102,39 @@ func (d *Data) Dedupe1() {
 
 // Dedupe - dedupe array of strings
 func (d *Data) Dedupe2() {
-	set := make(map[string]interface{})
-	var res []string
-	sort.Slice(d.words, func(i, j int) bool { return d.words[i] > d.words[j] })
-	// sort.Sort(sort.Reverse(sort.StringSlice{}d.words))
+	set := make(map[string]struct{})
+	var res = d.words[:0]
+	// sort.Slice(d.words, func(i, j int) bool { return d.words[i] > d.words[j] })
+	// sort.Sort(sort.Reverse(sort.StringSlice(d.words)))
+	slices.SortFunc(d.words, func(i, j string) int {
+		return cmp.Compare(len(j), len(i))
+	})
 
-	for _, sub := range d.words {
+	for _, word := range d.words {
+		fmt.Println("checking word", word)
 		// strs := strings.Split(s, " ")
 		// for _, sub := range s {
-		if _, ok := set[sub]; !ok {
-			set[sub] = nil
-			res = append(res, sub)
-		}
+		// for k := range set {
+		// 	fmt.Println("checking k", k)
+		// 	if !strings.Contains(k, word) {
+		// 		fmt.Println("new word", word, k)
+		// 		set[word] = struct{}{}
+		// 		res = append(res, word)
+		// 	}
 		// }
-	}
+		_, ok := set[word]
+		if !ok {
+			for k := range set {
+				fmt.Println("checking k", k)
+				if !strings.Contains(k, word) {
+					fmt.Println("new word", word, k)
+					set[word] = struct{}{}
+					res = append(res, word)
+				}
+			}
+		}
 
+	}
+	fmt.Printf("res %v\n", res)
 	d.WordsDeduped = res
 }
--- dedupe_test.go
+++ dedupe_test.go
@@ -2,65 +2,67 @@ package dedupe
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-// func BenchmarkDedupe(b *testing.B) {
-// 	words := []string{"abc", "bcd", "abcde", "cdefg", "bg"}
-// 	for range b.N {
+func BenchmarkDedupe(b *testing.B) {
+	words := []string{"abc", "bcd", "abcde", "cdefg", "bg"}
+	// for range b.N {
+	s := NewSet()
+	s.AddWords(words)
+	s.Dedupe2()
 
+	b.Log(s.Deduped())
+	// }
+}
+
+// func TestDedupeStrings(t *testing.T) {
+// 	tests := []struct {
+// 		name     string
+// 		input    []string
+// 		expected []string
+// 	}{
+// 		{
+// 			name:     "empty slice",
+// 			input:    []string{},
+// 			expected: []string{},
+// 		},
+// 		{
+// 			name:     "no duplicates",
+// 			input:    []string{"a", "b", "c"},
+// 			expected: []string{"a", "b", "c"},
+// 		},
+// 		{
+// 			name:     "with duplicates",
+// 			input:    []string{"abc", "bcd", "abcde", "cdefg", "bg"},
+// 			expected: []string{"abcde", "cdefg", "bg"},
+// 		},
+// 		{
+// 			name:     "all duplicates",
+// 			input:    []string{"a", "a", "a"},
+// 			expected: []string{"a"},
+// 		},
+// 		{
+// 			name:     "with empty strings",
+// 			input:    []string{"", "a", "", "b", ""},
+// 			expected: []string{"", "a", "b"},
+// 		},
+// 		{
+// 			name:     "with spaces",
+// 			input:    []string{" ", "a", " ", "b", "  "},
+// 			expected: []string{" ", "  ", "a", "b"},
+// 		},
+// 	}
+// 	s := NewSet()
+// 	for _, tt := range tests {
+// 		s.AddWords(tt.input)
+// 		s.Dedupe2()
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			result := s.Deduped()
+// 			t.Log("result", result)
+// 			assert.ElementsMatch(t, tt.expected, result)
+// 		})
 // 	}
 // }
-
-func TestDedupeStrings(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    []string
-		expected []string
-	}{
-		{
-			name:     "empty slice",
-			input:    []string{},
-			expected: []string{},
-		},
-		{
-			name:     "no duplicates",
-			input:    []string{"a", "b", "c"},
-			expected: []string{"a", "b", "c"},
-		},
-		{
-			name:     "with duplicates",
-			input:    []string{"abc", "bcd", "abcde", "cdefg", "bg"},
-			expected: []string{"abcde", "cdefg", "bg"},
-		},
-		{
-			name:     "all duplicates",
-			input:    []string{"a", "a", "a"},
-			expected: []string{"a"},
-		},
-		{
-			name:     "with empty strings",
-			input:    []string{"", "a", "", "b", ""},
-			expected: []string{"", "a", "b"},
-		},
-		{
-			name:     "with spaces",
-			input:    []string{" ", "a", " ", "b", "  "},
-			expected: []string{" ", "  ", "a", "b"},
-		},
-	}
-	s := NewSet()
-	for _, tt := range tests {
-		s.AddWords(tt.input)
-		s.Dedupe2()
-		t.Run(tt.name, func(t *testing.T) {
-			result := s.Deduped()
-			t.Log("result", result)
-			assert.ElementsMatch(t, tt.expected, result)
-		})
-	}
-}
 
 // func TestDedupeInts(t *testing.T) {
 // 	tests := []struct {
